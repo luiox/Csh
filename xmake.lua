8ead77c32b99cf35bbe97b865215c440c8c09538
Canrad <1517807724@qq.com>
2024-01-13 21:05:00 +0800

    fix:clangd can not find header files

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,9 @@
 add_rules("mode.debug", "mode.release")
 
+add_rules("plugin.compile_commands.autoupdate", { outputdir = "."})
+
 target("csh")
     set_kind("binary")
     add_includedirs("inc")
     add_files("src/**.c")
+
